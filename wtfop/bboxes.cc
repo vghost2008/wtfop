c8966e2366606b0bb73ee53270b5edbc97022b77
vghost <bluetornado@zju.edu.cn>
2019-05-10 15:52:03 +0800

    ...

--- wtfop/bboxes.cc
+++ wtfop/bboxes.cc
@@ -2344,3 +2344,77 @@ class BoxesMatchOp: public OpKernel {
 		float threshold_;
 };
 REGISTER_KERNEL_BUILDER(Name("BoxesMatch").Device(DEVICE_CPU).TypeConstraint<float>("T"), BoxesMatchOp<CPUDevice, float>);
+
+REGISTER_OP("AnchorGenerator")
+    .Attr("scales: list(float)")
+    .Attr("aspect_ratios:list(float)")
+	.Input("shape:int32")
+	.Input("size:int32")
+	.Output("anchors:float")
+	.SetShapeFn([](shape_inference::InferenceContext* c) {
+			c->set_output(0, c->Matrix(-1, 4));
+			return Status::OK();
+			});
+
+template <typename Device, typename T>
+class AnchorGeneratorOp: public OpKernel {
+	public:
+		explicit AnchorGeneratorOp(OpKernelConstruction* context) : OpKernel(context) {
+			OP_REQUIRES_OK(context, context->GetAttr("scales", &scales_));
+            vector<float> aspect_ratios;
+			OP_REQUIRES_OK(context, context->GetAttr("aspect_ratios", &aspect_ratios));
+            aspect_ratios_.assign(aspect_ratios.rbegin(),aspect_ratios.rend());
+		}
+
+		void Compute(OpKernelContext* context) override
+		{
+			const Tensor &_in_shape= context->input(0);
+			const Tensor &_in_size = context->input(1);
+			auto          in_shape = _in_shape.tensor<int,1>();
+			auto          in_size = _in_size.tensor<int,1>();
+
+			OP_REQUIRES(context, _in_shape.dims() == 1, errors::InvalidArgument("shape data must be 1-dimensional"));
+			OP_REQUIRES(context, _in_size.dims() == 1, errors::InvalidArgument("size data must be 1-dimensional"));
+            const int out_nr = scales_.size()*aspect_ratios_.size()*in_shape(0)*in_shape(1);
+
+			int dims_2d[2] = {out_nr,4};
+			TensorShape  outshape;
+			Tensor      *output_anchors = NULL;
+
+			TensorShapeUtils::MakeShape(dims_2d, 2, &outshape);
+
+			OP_REQUIRES_OK(context, context->allocate_output(0, outshape, &output_anchors));
+
+			auto oanchors = output_anchors->template tensor<T,2>();
+
+            oanchors.setZero();
+            auto index = 0;
+
+            const auto x_delta = 1.0/(in_shape(1));
+            const auto y_delta = 1.0/(in_shape(0));
+            auto y_pos = y_delta/2.0;
+            for(auto i=0; i<in_shape(0); ++i) {
+                auto x_pos = x_delta/2.0;
+                for(auto j=0; j<in_shape(1); ++j) {
+                    for(auto& a:aspect_ratios_) {
+                        for(auto& s:scales_) {
+                            const auto sa = sqrt(a);
+                            const auto hw = s/(2*in_size(1))/sa;
+                            const auto hh = s/(2*in_size(0))*sa;
+                            oanchors(index,0) = y_pos-hh;
+                            oanchors(index,1) = x_pos-hw;
+                            oanchors(index,2) = y_pos+hh;
+                            oanchors(index,3) = x_pos+hw;
+                            ++index;
+                        }
+                    }
+                    x_pos += x_delta;
+                }
+                y_pos += y_delta;
+            }
+		}
+	private:
+        std::vector<float> scales_;
+        std::vector<float> aspect_ratios_;
+};
+REGISTER_KERNEL_BUILDER(Name("AnchorGenerator").Device(DEVICE_CPU), AnchorGeneratorOp<CPUDevice, float>);
